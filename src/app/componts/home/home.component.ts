215830ae5e22c807bc8d60d5442ca4a0db369c41
Erlan Marinho <erlan.marinho1125@gmail.com>
2025-09-16 19:03:21 -0300

    creation of the news session

--- src/app/componts/home/home.component.ts
+++ src/app/componts/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
@@ -9,5 +10,28 @@ import { HeaderComponent } from '../header/header.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  currentSlide: number = 0;
+  slidesPerView: number = 3; // Padrão: 3 notícias por vez
 
+  @HostListener('window:resize')
+  onResize() {
+    const screenWidth = window.innerWidth;
+    this.slidesPerView = screenWidth <= 768 ? 1 : 3; // Ajusta para 1 notícia em telas menores
+  }
+
+  prevSlide() {
+    const carouselWrapper = document.querySelector('.carousel-wrapper') as HTMLElement;
+    const totalSlides = carouselWrapper.children.length;
+
+    this.currentSlide = (this.currentSlide - 1 + totalSlides) % totalSlides;
+    carouselWrapper.style.transform = `translateX(-${(this.currentSlide / this.slidesPerView) * 100}%)`;
+  }
+
+  nextSlide() {
+    const carouselWrapper = document.querySelector('.carousel-wrapper') as HTMLElement;
+    const totalSlides = carouselWrapper.children.length;
+
+    this.currentSlide = (this.currentSlide + 1) % totalSlides;
+    carouselWrapper.style.transform = `translateX(-${(this.currentSlide / this.slidesPerView) * 100}%)`;
+  }
 }
